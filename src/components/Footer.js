88fb3ef287f3d5dcb1ffc65913d73cebf711a754
Sarah Strange <sarah.strange@availity.com>
2020-02-12 12:26:16 -0500

    chore: update license year and add moment date to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
+import moment from 'moment';
+
+const year = moment().format('YYYY');
 
 const Footer = () => (
   <p className="text-center mt-3">
-    Made with <i className="icon icon-heart text-danger" /> by{" "}
-    <a href="http://www.availity.com">Availity</a> 2019
+    Made with <i className="icon icon-heart text-danger" /> by <a href="http://www.availity.com">Availity</a> {year}
   </p>
 );
 

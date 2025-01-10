d939954c693b798b029b7391dfa42998b1f77cf0
Jordan Young <jordan.young@availity.com>
2025-01-10 15:39:59 -0500

    docs: update alert message

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ const features = [
     description: (
       <>
         Our custom CSS kit that is built on top of Bootstrap 4.{' '}
-        <Alert severity="error">TThis library will be replaced by Element.</Alert>
+        <Alert severity="error">This library will be replaced by Element.</Alert>
       </>
     ),
     href: 'https://availity.github.io/availity-uikit',
